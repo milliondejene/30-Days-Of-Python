e1812a68d1496ce0aa66e38c115fe428028ed067
Million D <milliondejene@protonmail.com>
2024-04-22 09:23:22 +0300

    first commit

---
+++ day-1/data_types_example.py
@@ -0,0 +1,37 @@
+# Integer
+integer_number = 10
+
+# Float
+float_number = 3.14
+
+# Complex
+complex_number = 2 + 3j
+
+# String
+string_text = "Hello, World!"
+
+# Boolean
+is_python_cool = True
+
+# List
+list_numbers = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_colors = ("red", "green", "blue")
+
+# Set
+set_fruits = {"apple", "banana", "orange"}
+
+# Dictionary
+dictionary_person = {"name": "John", "age": 30, "city": "New York"}
+
+# Printing each data type
+print("Integer Number:", integer_number)
+print("Float Number:", float_number)
+print("Complex Number:", complex_number)
+print("String Text:", string_text)
+print("Boolean Value:", is_python_cool)
+print("List of Numbers:", list_numbers)
+print("Tuple of Colors:", tuple_colors)
+print("Set of Fruits:", set_fruits)
+print("Dictionary of a Person:", dictionary_person)
