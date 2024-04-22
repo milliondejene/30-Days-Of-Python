242bffda525469ad92e2ccbaf8d5e6d1a77682a3
Million D <milliondejene@protonmail.com>
2024-04-22 09:28:33 +0300

    euclidean distance

---
+++ day-1/euclidean_distance.py
@@ -0,0 +1,12 @@
+import math
+
+# Coordinates of the first point
+x1, y1 = 2, 3
+
+# Coordinates of the second point
+x2, y2 = 10, 8
+
+# Euclidean distance calculation
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print("The Euclidean distance between (2, 3) and (10, 8) is:", distance)
